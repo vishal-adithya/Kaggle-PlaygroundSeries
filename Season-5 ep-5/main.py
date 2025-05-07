47c13fc674820d168ff212cb1a4e28bc94a82952
Vishal Adithya.A (Macbook) <98593482+vishal-adithya@users.noreply.github.com>
2025-05-07 18:51:32 +0530

    Updated main.py

--- Season-5 ep-5/main.py
+++ Season-5 ep-5/main.py
@@ -5,4 +5,18 @@ Created on Wed May  7 18:43:57 2025
 
 @author: vishaladithyaa
 """
+import pandas as pd
+import numpy as np
+import os
 
+TRAIN_DF_FILEPATH = os.path.join("Data","train.csv")
+TEST_DF_FILEPATH = os.path.join("Data","test.csv")
+
+train_df = pd.read_csv(TRAIN_DF_FILEPATH)
+
+def Preprocessing(df):
+    df = df.set_index(df["id"])
+    df["Sex"] = df["Sex"].map(lambda x:0 if x=="male" else 1)
+    return df
+    
+preprocessed_df = Preprocessing(train_df)

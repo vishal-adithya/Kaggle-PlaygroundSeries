31265253f9aac91a7e87e062da8d83061b05e8d7
Vishal Adithya.A (Macbook) <98593482+vishal-adithya@users.noreply.github.com>
2025-10-08 07:05:35 +0530

    Update main.py

--- Season-5 ep-10/main.py
+++ Season-5 ep-10/main.py
@@ -3,11 +3,11 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split,RandomizedSearchCV
 import xgboost as xgb
 
 TRAIN_DF_FILEPATH = os.path.join("Data","train.csv")
-
+MODEL_FILEPATH = os.path.join("experiments","kps-s5-e10-reg-gbtree-rmse.json")
 
 train_df = pd.read_csv(TRAIN_DF_FILEPATH)
 train_df.set_index("id",inplace=True)
@@ -57,10 +57,11 @@ preprocessed_train_df.head()
 X = preprocessed_train_df.drop(columns = ["accident_risk"])
 y = preprocessed_train_df["accident_risk"]
 
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,shuffle=False,random_state=47)
+X_train,X_val,y_train,y_val = train_test_split(X,y,test_size=0.2,shuffle=False,random_state=47)
 dtrain = xgb.DMatrix(data = X_train,label = y_train)
+dval = xgb.DMatrix(data = X_val,label = y_val)
 
-reg = xgb.XGBRegressor(random_state= 47,booster = "gblinear")
+reg = xgb.XGBRegressor(random_state= 47,booster = "gbtree",early_stopping_rounds = 20)
 param_grid = {
     "n_estimators":[100,300,500],
     "max_depth":[4,6,8],
@@ -69,3 +70,48 @@ param_grid = {
     "subsample":[0.7,0.85,1.0]
 }
 
+
+rsv = RandomizedSearchCV(reg,
+                            param_distributions=param_grid,
+                            scoring = "neg_root_mean_squared_error",
+                            verbose=4,
+                            n_jobs=-1,
+                            n_iter=5,
+                            random_state=47,
+                            cv = 10
+                            )
+rsv.fit(X_train,y_train,eval_set = [(X_train,y_train),(X_val,y_val)])
+best_params = rsv.best_params_
+print(rsv.best_score_)
+
+reg = xgb.XGBRegressor(**best_params,random_state= 47,booster = "gbtree",early_stopping_rounds = 20)
+reg.fit(X_train,y_train,eval_set = [(X_train,y_train),(X_val,y_val)])
+
+
+
+def Plot_LearningCurves(model):
+    hist = model.evals_result()
+    
+    t_rmse = hist["validation_0"]["rmse"]
+    v_rmse = hist["validation_1"]["rmse"]
+    
+    epochs = np.arange(1,len(t_rmse)+1)
+    plt.figure(figsize = (8,5))
+    
+    plt.plot(epochs,t_rmse,label = "Train RMSE")
+    plt.plot(epochs,v_rmse,label = "Validation RMSE")
+    
+    plt.xlabel("No of Boosting Rounds")
+    plt.ylabel("RMSE")
+    plt.title("GbTree : Learning Curves")
+    
+    plt.legend()
+    plt.grid(True)
+    
+    plt.show()
+
+Plot_LearningCurves(reg)
+reg.save_model(MODEL_FILEPATH)
+
+model = xgb.XGBRegressor()
+model.load_model(MODEL_FILEPATH)
